5d8831f39b97dccf0a5dbb49266d145f3bc26cf9
Gustavo Eidt <gustavo.eidt@gmail.com>
2022-02-26 19:19:18 -0300

    simple scale

--- mui-poc/src/themes/scale.jsx
+++ mui-poc/src/themes/scale.jsx
@@ -1,13 +1,13 @@
-const baseUnit = 8;
+const baseUnit = 0.8;
 const minSize = 1;
 const maxSize = 8;
 
-export const getScale = ({ size = 1 }) => {
+export const getScale = (size) => {
   if (size >= maxSize) {
-    return maxSize * baseUnit;
+    return `${maxSize * baseUnit}rem`;
   }
-  if (size <= minSize) {
-    return minSize * baseUnit;
+  if (size < minSize) {
+    return `0.01rem`;
   }
-  return size * baseUnit;
+  return `${size * baseUnit}rem`;
 };
